bf62056301052b5a9bb984398933ceefed53aca1
jabatista <jaderson.morais@gmail.com>
2020-10-27 18:02:55 +0000

    Primeiro commit gatwayservice

---
+++ src/main/java/com/zuka/gatway/GatwayServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.zuka.gatway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatwayServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatwayServiceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zuka/gatway/GatwayServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuka.gatway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatwayServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
